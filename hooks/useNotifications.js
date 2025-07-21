7ac9e03688003fb694420cbfbfad021262faa5f2
Clodoaldo Favaro <clodoaldo.favaro@grin.co>
2025-07-21 00:52:51 -0300

    fix: import react query from tanstack

--- hooks/useNotifications.js
+++ hooks/useNotifications.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { io } from 'socket.io-client';
 import toast from 'react-hot-toast';
 
